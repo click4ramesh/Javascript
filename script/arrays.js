3fad29aedbc4430e6d970382e73f44c00d4f4a19
click4ramesh <41024358+click4ramesh@users.noreply.github.com>
2020-11-13 17:08:51 -0500

    Add files via upload

---
+++ script/arrays.js
@@ -0,0 +1,30 @@
+// let arrayValues = ['one', 'two','three'];
+
+// const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+// let listItem = famousSayings[0];
+// console.log(listItem);
+// console.log(famousSayings[2]);
+// console.log(famousSayings[3]);
+
+
+// const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
+// console.log(objectives.length);
+
+// const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+// groceryList.shift( );
+// groceryList.unshift('popcorn');
+
+// console.log(groceryList.slice(1,3));
+
+
+//NEsted Arrays
+const nestedArr = [[1], [2, 3]];
+
+console.log(nestedArr[1]); // Output: [2, 3]
+console.log(nestedArr[1][0]); // Output: 2
+
+const numberClusters = [[1,2],[3,4],[5,6]];
+const target = numberClusters[2][2];
+
+
---
+++ script/classes.js
@@ -0,0 +1,57 @@
+// class Dog {
+//     constructor(name,behavior) {
+//       this._name = name;
+//       this._behavior = behavior;
+//     }
+  
+//     get name() {
+//       return this._name;
+//     }
+//     get behavior() {
+//       return this._behavior;
+//     }   
+  
+//     get name_behavior() {
+//         return this._name, this._behavior;
+//     }   
+
+//     incrementBehavior() {
+//       this._behavior ++;
+//     }SDure
+//   }
+  
+
+  
+//   const halley = new Dog("Halley",100);
+//   console.log(halley);
+//   console.log(halley._behavior);
+//   console.log(halley._name);
+//   console.log(halley.name_behavior);
+
+// class Surgeon {
+//     constructor(name, department) {
+//       this.name = name;
+//       this.department = department;
+//     }
+//   }
+  
+//   const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+
+class Book{
+
+
+}
+
+class CD{
+
+}
+
+class Movie{
+
+
+}
+
+class Media{
+
+  
+}
